35e3fdccb9244c9374c7b25c5c5d7e1326c702e8
LENOVO <LENOVO@LAPTOP-IGNO6GRG>
2024-03-05 10:34:36 +0700

    Add project files.

---
+++ TestShopMoblie/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestShopMoblie
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
